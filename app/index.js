0d945f5819b85217abfe1863a37741f71de9a40a
Leif Anderson <leif.v.anderson@gmail.com>
2018-07-11 10:11:05 -0500

    update

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@
 
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer(function(req,res){
     //get url
@@ -13,10 +14,18 @@ const server = http.createServer(function(req,res){
     //get path
     let path = parsedUrl.pathname;
     let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+    // get query string as an obj
+    let queryStringObj = parsedUrl.query;
+    // get header as obj
+    let headers = req.headers;
+    let decoder = new StringDecoder('utf-8')
+    // get http method
+    let method = req.method.toLowerCase();
     //send response
     res.end('yo bro');
     // log path
-    console.log('request received on path: '+trimmedPath);
+    console.log('path: '+trimmedPath+'method: '+method+'query: ', queryStringObj);
+    console.log('header: ', headers);
 });
 
 server.listen(3000, function(){
